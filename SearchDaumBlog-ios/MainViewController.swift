11f5dbab9877bb76509bfa680e1331f22b8d3376
jinan <ja.choi@sweetk.co.kr>
2023-05-31 23:07:12 +0900

    1. SceneDelegate에 메인뷰 관련 초기설정 완료

---
+++ SearchDaumBlog-ios/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  SearchDaumBlog-ios
+//
+//  Created by 최진안 on 2023/05/31.
+//
+
+import RxSwift
+import RxCocoa
+
+class MainViewController: UIViewController {
+    
+}
